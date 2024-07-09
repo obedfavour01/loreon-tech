1df98386cfe987cb76c80c91b9f7ca2efc0e72e8
obedfavour01 <obedfavour01@gmail.com>
2024-07-09 23:09:00 +0100

    new changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,23 +8,23 @@ import Footer from "@/components/Landing/Footer/Footer";
 import {  Arimo, Inter, Poppins, Roboto, Syne } from "next/font/google"
 import Navbar from '@/components/Navbar';
 
-export const RobotoFonts = Roboto({
+ const RobotoFonts = Roboto({
     weight: ["100","300","400","700","900"],
     subsets:['latin'],
     variable: "--font-roboto"
 })
   
-  export const Pop = Poppins({
+   const Pop = Poppins({
     weight: ["400","500","600","700","800"],
     subsets:["latin-ext"]}
   )
   
-  export const InterFonts = Inter({
+   const InterFonts = Inter({
     weight: ["400","500","600","700","800"],
     subsets:["latin"]}
   )
   
-export const Coolvetica = localFont({
+ const Coolvetica = localFont({
   src: './fonts/coolvetica rg.ttf',
   display: 'swap',
   variable: '--font-coolvetica'
